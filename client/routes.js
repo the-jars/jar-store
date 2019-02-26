31328f742aeef56f27e4557797c00d79ec048781
Jake H <43487010+greatheights82@users.noreply.github.com>
2019-02-26 17:19:44 -0600

    implement Single product (#48)
    * Initial creation of product route file
    
    * switching branches to help with DB issue
    
    * Route: singleProduct /GET route
    
    * pulling updated master
    
    * /products/:id route tested in Postman
    
    * Module: Single Product View;
    
    * recommitting with seed data, component not functional
    
    * Feature: Single Product view actually works

--- client/routes.js
+++ client/routes.js
@@ -4,6 +4,7 @@ import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
 import {Login, Signup, UserHome} from './components'
 import {me} from './store'
+import singleProduct from './components/singleProduct'
 
 /**
  * COMPONENT
@@ -28,6 +29,7 @@ class Routes extends Component {
           </Switch>
         )}
         {/* Displays our Login component as a fallback */}
+        <Route path="/products/:id" component={singleProduct} />
         <Route component={Login} />
       </Switch>
     )
