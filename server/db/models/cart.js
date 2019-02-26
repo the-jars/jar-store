8db0fd57bcb364d63063fe34291fa365adf9e802
Jake Hebert <greatheights@gmail.com>
2019-02-26 11:05:36 -0600

    lowercased cart.id

--- server/db/models/cart.js
+++ server/db/models/cart.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const Cart = db.define('cart', {
-  Id: {
+  id: {
     type: Sequelize.INTEGER,
     autoIncrement: true,
     primaryKey: true
