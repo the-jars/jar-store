792997a131f850947b330e3b4ebba6310c7f5c01
Jake Hebert <greatheights@gmail.com>
2019-02-26 11:56:54 -0600

    switching branches to help with DB issue

---
+++ server/api/product.js
@@ -0,0 +1,10 @@
+const router = require('express').Router()
+const sequelize = require('sequelize')
+const {Product} = require('../db/models.index.js')
+
+// // GET /api/products/:id
+// router.get('/:id', async (req, res, next) => {
+//   const singleProduct = await
+// })
+
+module.exports = router
