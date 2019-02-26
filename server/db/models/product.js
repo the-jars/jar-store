80e67d208e0780c7c32158bcc2bb234cb5745f87
Seoung_Yeon Lee <alexlee131005@gmail.com>
2019-02-26 10:37:32 -0600

    finish defining product model

---
+++ server/db/models/product.js
@@ -0,0 +1,36 @@
+const Sequelize = require("sequelize")
+const db = require("../db")
+
+const Product = db.define('product', {
+  // name of the product
+  // string, not nullable and not ''(empty string)
+  name: {
+    type:Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  // price in decimal
+  // max length is 10
+  // decimal precision is 2
+  price: {
+    type:Sequelize.DOUBLE(10,2),
+    defaultValue: 0.00
+  },
+  // boolean for availability set by admin
+  // true when available
+  // false when not available
+  status: {
+    type:Sequelize.BOOLEAN,
+    defaultValue: true
+  }
+}, {
+  getterMethods: {
+    availability() {
+      return this.status? "available":"not available"
+    }
+  }
+})
+
+module.exports = Product
