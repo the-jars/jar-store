5d6519d345acde8c938e278ed6afc360611709ce
Jake Hebert <greatheights@gmail.com>
2019-02-26 12:06:00 -0600

    Route: singleProduct /GET route

--- server/api/product.js
+++ server/api/product.js
@@ -2,9 +2,17 @@ const router = require('express').Router()
 const sequelize = require('sequelize')
 const {Product} = require('../db/models.index.js')
 
-// // GET /api/products/:id
-// router.get('/:id', async (req, res, next) => {
-//   const singleProduct = await
-// })
+// GET /api/products/:id
+router.get('/:id', async (req, res, next) => {
+  try {
+    const singleProduct = await Product.findById(req.params.id)
+    if (!singleProduct) {
+      res.sendStatus(500)
+    }
+    res.json(singleProduct)
+  } catch (error) {
+    next(error)
+  }
+})
 
 module.exports = router
