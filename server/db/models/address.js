7b815f81abd6185413e98ab75037b0f5d06aee6d
Rose Graner <rosegraner@gmail.com>
2019-02-26 10:40:27 -0600

    user and address models

---
+++ server/db/models/address.js
@@ -0,0 +1,33 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Address = db.define('address', {
+  type: {
+    type: Sequelize.dataType
+  },
+  line_1: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  line_2: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  city: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  state: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  zip: {
+    type: Sequelize.STRING,
+    allowNull: false
+  }
+  // user: {
+  //  Address.hasOne(User)
+  // },
+})
+
+module.exports = Address
