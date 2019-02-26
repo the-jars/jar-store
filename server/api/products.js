f1af5eea32f0467c907ec0fde98d785ebf2de05c
Seoung_Yeon Lee <alexlee131005@gmail.com>
2019-02-26 17:45:10 -0600

    implement put and post route for product

--- server/api/products.js
+++ server/api/products.js
@@ -1,7 +1,18 @@
 const router = require('express').Router()
 const {Product, Category} = require('../db/models/index.js')
 
+// param for getting the required product
+router.param('id', (req, res, next, id) =>
+  Product.findById(id, {include: [Category]})
+    .then(product => {
+      req.product = product
+      next()
+    })
+    .catch(console.log)
+)
+
 // GET /api/products/:id
+/** TODO: fix to utilize param */
 router.get('/:id', async (req, res, next) => {
   try {
     const singleProduct = await Product.findById(req.params.id, {
@@ -15,6 +26,25 @@ router.get('/:id', async (req, res, next) => {
   } catch (error) {
     next(error)
   }
+})
+
+// for updating existing product
+router.put('/:id', (req, res, next) => {
+  const updateField = req.body
+  req.product
+    .update(updateField)
+    .then(updatedProduct => res.status(201).send(updatedProduct))
+    // handle when user is not admin
+    .catch(next)
+})
+/**
+ * used for creating new product
+ */
+router.post('/', (req, res, next) => {
+  const createField = req.body
+  Product.create(createField)
+    .then(createdProduct => res.status(201).send(createdProduct))
+    .catch(next)
 })
 
 module.exports = router
