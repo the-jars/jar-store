a0eef07742fe13d7c9c2ff1cf9ac22a4c5f95c8e
SteffeniV <steffenibunsick@gmail.com>
2019-02-27 17:31:11 -0600

    adds link to jars in navbar

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -15,6 +15,7 @@ const Navbar = ({handleClick, isLoggedIn}) => (
           <a href="#" onClick={handleClick}>
             Logout
           </a>
+          <Link to="/products">Jars!</Link>
         </div>
       ) : (
         <div>
