fa8d302113f4ea43685e466726686a2040c37f3f
Seoung_Yeon Lee <alexlee131005@gmail.com>
2019-02-28 15:54:47 -0600

    commit before merging master

--- server/api/carts.js
+++ server/api/carts.js
@@ -3,8 +3,9 @@ const {Cart, CartItem} = require('../db/models')
 
 // Routes for /api/carts
 
-router.param('cartId', (req, res, next, id) =>
-  Cart.findById(id, {include: [CartItem]})
+// when cartId is provided, grab that cart and move onto the next route
+router.param('cartId', (req, res, next, cartId) =>
+  Cart.findById(cartId, {include: [CartItem]})
     .then(cart => {
       req.cart = cart
       next()
@@ -13,6 +14,36 @@ router.param('cartId', (req, res, next, id) =>
     .catch(next)
 )
 
-//router.delete('/:cartId', (req, res, next) => )
+router.param('itemId', (req, res, next, itemId) =>
+  Cart.findById(itemId)
+    .then(item => {
+      req.item = item
+      next()
+      return null
+    })
+    .catch(next)
+)
+
+/** routes for deleting cartItem from the cart
+ * - happens after router.param('cartId')
+ * - recives id of cartItem to remove through the body as itemId
+ * - uses magic method on the cart instance to unassociate the specific item
+ * - after, remove that item from the database cartItem model
+ */
+router.delete('/:cartId/:itemId', (req, res, next) =>
+  req.cart
+    .removeCartitem(req.item.id)
+    // if item was succesfully removed, also delete it from the cartItem
+    // send status for succesfully deleted: 204
+    .then(() => {
+      req.item
+        .destroy()
+        .then(() => {
+          res.status(204).send(req.cart.cartitems)
+        })
+        .catch(next)
+    })
+    .catch(next)
+)
 
 module.exports = router
--- server/api/carts.spec.js
+++ server/api/carts.spec.js
@@ -3,3 +3,49 @@ const request = require('supertest')
 const db = require('../db')
 const app = require('../index')
 const Cart = db.model('cart')
+const CartItem = db.model('cartitem')
+const Product = db.model('product')
+
+describe('Cart routes', () => {
+  // set up cart route test
+  // the test cart has one fart jar and two barf jar pre-loaded
+  let cart
+  let fartJar
+  let barfJar
+  let oneFartJar
+  let twoBarfJar
+  beforeEach(async () => {
+    await db.sync({force: true})
+    cart = await Cart.create()
+    fartJar = await Product.create({name: 'fart jar'})
+    barfJar = await Product.create({name: 'barf jar'})
+    // putting one 'Fart Jar' into the cart
+    oneFartJar = await CartItem.create({quantity: 1})
+      .then(async item => {
+        await item.setProduct(fartJar)
+        await item.setCart(cart)
+        await cart.addCartitem(item)
+        return item
+      })
+      .catch(console.log)
+    // putting two 'Barf Jar' into the cart
+    twoBarfJar = await CartItem.create({quantity: 2})
+      .then(async item => {
+        await item.setProduct(barfJar)
+        await item.setCart(cart)
+        await cart.addCartitem(item)
+        return item
+      })
+      .catch(console.log)
+  })
+
+  describe('DELETE /api/carts/:cartId/:itemId', () => {
+    it('deletes the item from the cartItem', async () => {
+      const res = await request(app)
+        .delete(`/api/carts/${cart.id}/${oneFartJar.id}`)
+        .expect(204)
+      console.log(res.data)
+      expect(res.data).to.be.equal([oneFartJar, twoBarfJar])
+    })
+  }) // end describe('DELETE /api/carts/:cartId')
+})
