a56f3d8b647df213ae59206cca0cd80b1607fc0c
Seoung_Yeon Lee <alexlee131005@gmail.com>
2019-02-28 11:35:05 -0600

    initial commit

--- server/api/carts.js
+++ server/api/carts.js
@@ -3,4 +3,16 @@ const {Cart, CartItem} = require('../db/models')
 
 // Routes for /api/carts
 
+router.param('cartId', (req, res, next, id) =>
+  Cart.findById(id, {include: [CartItem]})
+    .then(cart => {
+      req.cart = cart
+      next()
+      return null
+    })
+    .catch(next)
+)
+
+//router.delete('/:cartId', (req, res, next) => )
+
 module.exports = router
