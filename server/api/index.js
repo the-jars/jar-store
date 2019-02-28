871363b4d4c4bc2a55ccedd1b9778fe23f574b4e
Jake H <43487010+greatheights82@users.noreply.github.com>
2019-02-28 16:35:21 -0600

    Cart find or instantiate (#62)
    * POST/api/carts finds an active cart or creates a new one
    
    * cart reducer: action-setCart, thunk-fetchOrCreateCart
    
    * changed post route to only findOrCreat (no eager loading, no setting state
    
    * fixed post route to send status 200 instead of empty obj
    
    * Cart post route updated to pull based on sessionId if user is not logged in

--- server/api/index.js
+++ server/api/index.js
@@ -7,7 +7,6 @@ router.use('/categories', require('./categories'))
 router.use('/carts', require('./allcart'))
 router.use('/carts', require('./carts'))
 
-
 router.use((req, res, next) => {
   const error = new Error('Not Found')
   error.status = 404
