b1a0f158315e49aa6f4a1c22c4e2d86dd058ea54
Jake Hebert <greatheights@gmail.com>
2019-02-28 12:12:44 -0600

    POST/api/carts finds an active cart or creates a new one

--- server/api/carts.js
+++ server/api/carts.js
@@ -3,4 +3,21 @@ const {Cart, CartItem} = require('../db/models')
 
 // Routes for /api/carts
 
+// POST/api/carts
+router.post('/', async (req, res, next) => {
+  try {
+    const response = await Cart.findOrCreate({
+      where: {
+        userId: req.body.userId,
+        status: 'active'
+      },
+      include: [CartItem]
+    })
+    const userCart = response[1]
+    res.json(userCart)
+  } catch (error) {
+    next(error)
+  }
+})
+
 module.exports = router
