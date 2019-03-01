27c79012ba8aabc9a4655a745464c35ba55ce824
Alex "Seoung Yeon" Lee <alexlee131005@gmail.com>
2019-03-01 14:59:28 -0600

    Delete cart item (#35)
    * add test spec for the deleteCartItem feature
    
    * separate cart redux tests into smaller chunk
    
    * hooks up delete route to the button in cart view
    
    * combine /api/carts route and fix carts route test
    
    * remove unneeded console.log

---
+++ client/store/cart.spec.js
@@ -0,0 +1,48 @@
+import {expect} from 'chai'
+import {deleteItem, deleteCartItem} from './cart'
+import axios from 'axios'
+import MockAdapter from 'axios-mock-adapter'
+import configureMockStore from 'redux-mock-store'
+import thunkMiddleware from 'redux-thunk'
+
+const middlewares = [thunkMiddleware]
+const mockStore = configureMockStore(middlewares)
+
+describe('Cart redux state', () => {
+  let store
+  let mockAxios
+  const fakeItem1 = {cartId: 1, productId: 1}
+
+  // the cart is an array of cartItems
+  const initialState = {cart: [fakeItem1]}
+
+  beforeEach(() => {
+    mockAxios = new MockAdapter(axios)
+    store = mockStore(initialState)
+    console.log(store.getState())
+  })
+
+  afterEach(() => {
+    mockAxios.restore()
+    store.clearActions()
+  })
+
+  describe('deleting items from cart', () => {
+    describe('deleteCartItem thunk', () => {
+      it('makes axios request to /api/carts/1/1', async () => {
+        // set-up fake axios
+        mockAxios.onDelete('/api/carts/1/1').replyOnce(204)
+        const status = await store.dispatch(deleteCartItem(fakeItem1))
+        expect(status).to.be.equal(204)
+      })
+
+      it('dispatches deleteItem action creator which returns the correct action', async () => {
+        mockAxios.onDelete('/api/carts/1/1').replyOnce(204)
+        await store.dispatch(deleteCartItem(fakeItem1))
+        const actions = store.getActions()
+        expect(actions[0].type).to.be.equal('DELETE_ITEM')
+        expect(actions[0].itemToDelete).to.be.equal(fakeItem1)
+      })
+    }) // END describe('deleteCartItem thunk')
+  }) // END describe('deleting items from cart')
+})
--- server/api/carts.js
+++ server/api/carts.js
@@ -1,7 +1,5 @@
 const router = require('express').Router()
-const {Cart} = require('../db/models')
-
-// Routes for /api/carts
+const {Cart, CartItem, Product} = require('../db/models')
 
 // POST/api/carts
 router.post('/', async (req, res, next) => {
@@ -22,5 +20,67 @@ router.post('/', async (req, res, next) => {
     next(error)
   }
 })
+// when cartId is provided, grab that cart and move onto the next route
+router.param('cartId', (req, res, next, cartId) =>
+  Cart.findById(cartId, {include: [CartItem]})
+    .then(cart => {
+      req.cart = cart
+      next()
+      return null
+    })
+    .catch(next)
+)
 
+router.param('itemId', (req, res, next, itemId) =>
+  CartItem.findById(itemId)
+    .then(item => {
+      req.item = item
+      next()
+      return null
+    })
+    .catch(next)
+)
+
+/** route for deleting cartItem from the cart
+ * - happens afte dr router.param('cartId')
+ * - recives id of cartItem to remove through the body as itemId
+ * - uses magic method on the cart instance to unassociate the specific item
+ * - after, remove that item from the database cartItem model
+ */
+router.delete('/:cartId/:itemId', (req, res, next) =>
+  req.cart
+    .removeCartitem(req.item.id)
+    // if item was succesfully removed, also delete it from the cartItem
+    // send status for succesfully deleted: 204
+    .then(() => req.item.destroy())
+    .then(() => res.sendStatus(204))
+    .catch(next)
+)
+
+// Routes for /api/carts
+//GET /api/cart for getting cart by the logged in user
+router.get('/:userId', async (req, res, next) => {
+  try {
+    //pull cartId with userid && active in cart table
+    // console.log('id', req.body.userId)
+    const cart = await Cart.findOne({
+      where: {
+        userId: req.params.userId,
+        status: 'active'
+      }
+    })
+    const cartId = cart.id
+    //pull everything from cartitems with cartid
+    const items = await CartItem.findAll({
+      where: {
+        cartId: cartId
+      },
+      //eager load product info
+      include: [{model: Product}]
+    })
+    res.json(items)
+  } catch (err) {
+    next(err)
+  }
+})
 module.exports = router
