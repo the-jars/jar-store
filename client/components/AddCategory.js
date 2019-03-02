3558eb83b3f320044bc99453ec91493d8d600460
Rose Graner <rosegraner@gmail.com>
2019-03-02 16:38:49 -0600

    adds a row to category table. renders add category component when add category button is clicked on productslist page.

---
+++ client/components/AddCategory.js
@@ -0,0 +1,52 @@
+import {addCategoryThunk, fetchCategories} from '../store/category.js'
+import {connect} from 'react-redux'
+import {Button, Form} from 'semantic-ui-react'
+import React, {Component} from 'react'
+
+class AddCategory extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: ''
+    }
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleSubmit = event => {
+    event.preventDefault()
+    this.props.addCategory(this.state)
+    this.setState({
+      name: ''
+    })
+    this.props.getCategories()
+  }
+
+  handleChange = event => {
+    console.log(this.state)
+    return this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+  render() {
+    return (
+      <Form onChange={this.handleChange} onSubmit={this.handleSubmit}>
+        <Form.Input
+          type="text"
+          name="name"
+          id="name"
+          value={this.state.name}
+          onChange={this.handleChange}
+        />
+        <Button type="submit">Add Category</Button>
+      </Form>
+    )
+  }
+}
+
+const mapDispatchToProps = dispatch => ({
+  addCategory: newCategoryInfo => dispatch(addCategoryThunk(newCategoryInfo)),
+  getCategories: () => dispatch(fetchCategories())
+})
+
+export default connect(null, mapDispatchToProps)(AddCategory)
--- client/store/category.js
+++ client/store/category.js
@@ -1,10 +1,16 @@
 import axios from 'axios'
 
 const SET_CATEGORIES = 'SET_CATEGORIES'
+const ADD_CATEGORY = 'ADD_CATEGORY'
 
 export const setCategories = categories => ({
   type: SET_CATEGORIES,
   categories
+})
+
+export const addCategory = category => ({
+  type: ADD_CATEGORY,
+  category
 })
 
 export const fetchCategories = () => dispatch =>
@@ -13,9 +19,18 @@ export const fetchCategories = () => dispatch =>
     .then(response => dispatch(setCategories(response.data)))
     .catch(console.log)
 
+export const addCategoryThunk = newCategoryInfo => dispatch => {
+  axios
+    .post(`/api/categories`, newCategoryInfo)
+    .then(res => dispatch(addCategory(res.data)))
+    .catch(console.log)
+}
+
 export const categories = (state = [], action) => {
   if (action.type === SET_CATEGORIES) {
     return action.categories
+  } else if (action.type === ADD_CATEGORY) {
+    return [...state, action.category]
   }
   return state
 }
